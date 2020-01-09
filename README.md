6b57c3516d22c138710c07b123fda25c693ec60a
Muthuraja.Ganesan <muthuraja.ganesan@raqmiyat.com>
2020-01-09 19:26:02 +0530

    Minir Changes

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# CustomAlert 
+---
+title: Ionic Network Alert 
+description: Device's network status notification
+---
+
+# cordova-plugin-raqmiyat-alert
+
+
+## Installation
+    cordova plugin add cordova-plugin-raqmiyat-alert (or)
+    cordova plugin add https://github.com/GMuthuraja/IonicNetworkAlert.git
+
+## Supported Platforms
+- Android
+
+### How to Use
+```
+declare var CustomAlert: any; //paste it below the import section
+
+checkStatus() { //function call
+CustomAlert.coolMethod((response) => {
+      console.log(response);
+    }, (error => {
+      console.log(error);
+})
+}
+```
--- src/android/customalert.java
+++ src/android/customalert.java
@@ -11,34 +11,51 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 
 
+//Main Java class which should inherit CordovaPlugin
 public class CustomAlert extends CordovaPlugin{
 
+    //action  which will come from js file on www folder
     public static final String ACTION_DISPLAY_ALERT = "displayAndroidAlert";
 
+    //plugin function which will invoked when user call the plugin in ionic 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext){
      try{
             if(ACTION_DISPLAY_ALERT.equals(action)){
+                //Connectivity managet to get the device network information
                 ConnectivityManager conn = (ConnectivityManager) this.cordova.getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo info = conn.getActiveNetworkInfo();
                 AlertDialog.Builder dialog = new AlertDialog.Builder(this.cordova.getActivity());
 
+                //Check whether network connected or not
                 if(info != null && info.isConnected()) {
                     dialog.setTitle("Network Connected");
-                    dialog.setMessage("You have connected to the network!");
+                    dialog.setMessage("You are online now!");
                 }else{
                     dialog.setTitle("No Network Access");
                     dialog.setMessage("Please check your network connection!");
                 }
-                
+
+                //Alert button
+                dialog.setPositiveButton("CLOSE", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id) {
+                        //do thing
+                    }
+                });
+
+                //this will show alert dialog
                 dialog.show();
-                callbackContext.success("connected");
+
+                //success callback 
+                callbackContext.success("success");
                 return true;
             }else {
-                callbackContext.error("wrong method");
+                //error callback
+                callbackContext.error("Something went wrong!");
                 return false;
             }
         }catch(Exception e){
+            //error callback
             callbackContext.error(e.getMessage());
             return false;
         }
